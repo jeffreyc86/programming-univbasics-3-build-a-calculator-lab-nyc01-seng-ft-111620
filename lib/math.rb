ca3c79df60bdba4d6c957c970ba7742c3a859f7c
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-29 16:15:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,18 +7,25 @@ end
 def subtraction(num1, num2)
   difference = num1 - num2
   puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  puts quotient
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
+  remainder
 end
 
 def square_root(num)
